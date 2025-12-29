5609a98bda1ae80499cc2057e1d36fc9e2748aa4
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-12-29 21:30:02 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "21499867"))
-API_HASH = environ.get("API_HASH", "1a24a802a74f0e3a50dfe5ce72ccd8cd")
+API_ID = int(environ.get("API_ID", "27765349"))
+API_HASH = environ.get("API_HASH", "9df1f705c8047ac0d723b29069a1332b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "8203195968"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+OWNER = int(environ.get("OWNER", "1116405290"))
+CREDIT = environ.get("CREDIT", "Star AJ")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '12345').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '1116405290').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '12345').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '1116405290').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
@@ -23,4 +23,5 @@ if int(OWNER) not in AUTH_USERS:
 api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
